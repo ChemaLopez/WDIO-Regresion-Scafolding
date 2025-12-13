e2944221c638080fd3512deebde3309d930bc60e
ChemaLopez <chemalop@hotmail.es>
2025-12-13 15:45:15 +0100

    chore: add ts, last wdio config, node env files

--- test/specs/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-
-describe('Página de ejemplo', () => {
-  it('debe abrir webdriver.io y comprobar el título', async () => {
-    await browser.url('https://webdriver.io');
-    const title = await browser.getTitle();
-    expect(title.toLowerCase()).to.include('webdriver');
-  });
-});
